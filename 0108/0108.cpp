5f1fae3c5001bfbb3801fadb1eb6c7e51381e281
Kwag De Gi <code@skyaperture.com>
2015-01-08 09:27:37 +0900

    Constant

--- 0108/0108.cpp
+++ 0108/0108.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+const double pi = 3.14159;
+const char NL = '\n';
 
 int main() 
 {
-	std::cout << "Hi World";
+	int a = 5;
+	int b(3);
+	int c{2};
+	string d;
+	
+	cout << "Hi World\n";
+
+	a = a + b;
+	d = "This is a string\n";
+	cout << d;
+	return 0;
 }
