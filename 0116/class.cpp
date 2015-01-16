16c9b096414e4e035f89289da05505f67e247a89
Kwag De Gi <code@skyaperture.com>
2015-01-16 13:04:49 +0900

    class for box with member function

--- 0116/class.cpp
+++ 0116/class.cpp
@@ -7,7 +7,30 @@ class Box
 		double length;
 		double height;
 		double breadth;
+		void setLength( double len );
+		void setHeight( double hei );
+		void setBreadth( double bre );
+		double getVolume(void)
+		{
+			return length * breadth * height;
+		}
+
 };
+
+void Box::setLength( double len )
+{
+	length = len;
+}
+
+void Box::setHeight( double hei )
+{
+	height = hei;
+}
+
+void Box::setBreadth( double bre )
+{
+	breadth = bre;
+}
 
 int main()
 {
@@ -15,9 +38,9 @@ int main()
 	Box Box2;
 	double volume = 0.0;
 
-	Box1.height = 5.0;
-	Box1.length = 6.0;
-	Box1.breadth = 7.0;
+	Box1.setHeight( 5.0 );
+	Box1.setLength( 6.0 );
+	Box1.setBreadth( 7.0 );
 
 	Box2.height = 10.0;
 	Box2.length = 12.0;
@@ -26,6 +49,8 @@ int main()
 	volume = Box1.height * Box1.length * Box1.breadth;
 	cout << "Volume of Box1 " << volume << endl;
 
-
+	volume = Box2.getVolume();
+	cout << "Volume of Box2 " << volume << endl;
+	
 	return 0;
 }
