94a7f2089a2f220264258b87e51f384cbed07f3a
Kwag De Gi <code@skyaperture.com>
2015-01-21 22:17:14 +0900

    constructor overload

--- 0121/main.cpp
+++ 0121/main.cpp
@@ -5,9 +5,15 @@ using namespace std;
 class Rectangle {
 	int width, height;
 	public :
+	Rectangle ();
 	Rectangle (int,int);
 	int area() { return width*height; }
 };
+
+Rectangle::Rectangle () {
+	width = 5;
+	height = 5;
+}
 
 Rectangle::Rectangle (int x, int y) {
 	width = x;
@@ -15,8 +21,9 @@ Rectangle::Rectangle (int x, int y) {
 }
 
 int main() {
-	Rectangle rect(3,4), rectb(5,6); 
+	Rectangle rect(3,4), rectb(5,6), rectc; 
 	cout << "area : " << rect.area() << endl;
 	cout << "area : " << rectb.area() << endl;
+	cout << "area : " << rectc.area() << endl;
 	return 0;
 }
