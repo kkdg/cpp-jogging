0d9c2ac346c2493e8a0c83225207f98a7d931442
Kwag De Gi <code@skyaperture.com>
2015-01-25 11:17:08 +0900

    constant objects

---
+++ 0125/const2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+	int x;
+    public:
+	MyClass(int val) : x(val) {}
+	const int& get() const { return x; }
+};
+
+void print (const MyClass& arg) {
+	cout << arg.get() << endl;
+}
+
+int main() {
+	MyClass foo(10);
+	print(foo);
+
+	return 0;
+}
