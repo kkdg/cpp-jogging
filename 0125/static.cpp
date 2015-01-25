4ab8501522484b1ea362184e4ad64cea46772d6f
Kwag De Gi <code@skyaperture.com>
2015-01-25 10:48:23 +0900

    static in cpp

---
+++ 0125/static.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Dummy {
+    public:
+	static int n;
+	Dummy () { n++; };
+	~Dummy () { n--; };
+};
+
+int Dummy::n=0;
+
+int main() {
+	Dummy a;
+	Dummy b[5];
+	Dummy * c = new Dummy;
+	cout << a.n << endl;
+	delete c;
+	cout << Dummy::n << endl;
+
+	return 0;
+}
